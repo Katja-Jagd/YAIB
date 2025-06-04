65c039044e292f9848a14ff3e84155bb46e5f707
Katja-Jagd <knjagd@hotmail.com>
2025-06-04 14:06:21 +0200

    adding evaluation script and tesing new parameters

--- HPC_script_SL.sh
+++ HPC_script_SL.sh
@@ -15,7 +15,7 @@
 ### -- specify that we want the job to get killed if it exceeds 5 GB per core/slot -- 
 #BSUB -M 4GB
 ### -- set walltime limit: hh:mm -- 
-#BSUB -W 48:00 
+#BSUB -W 05:00 
 ### -- set the email address -- 
 # please uncomment the following line and put in your e-mail address,
 # if you want to receive e-mail notifications on a non-default address
@@ -34,4 +34,4 @@ module load python3/3.10.16
 source yaib_venv/bin/activate
 
 # Execute command
-icu-benchmarks train     -d /work3/s185395/YAIB-cohorts/data/mortality24/mimic   -n mimic     -t BinaryClassification     -tn Mortality24     -m BAT     -gc     -lc -s 2222     -l ../yaib_logs/ --tune --wandb-sweep
+icu-benchmarks train     -d /work3/s185395/YAIB-cohorts/data/mortality24/miiv   -n miiv     -t BinaryClassification     -tn Mortality24     -m BAT     -gc     -lc -s 2222     -l ../yaib_logs/ --wandb-sweep
--- HPC_script_SL_eval.sh
+++ HPC_script_SL_eval.sh
@@ -2,7 +2,7 @@
 #!/bin/sh 
 ### General options  
 ### -- specify queue -- 
-#BSUB -q gpua100
+#BSUB -q gpuv100
 ### -- set the job Name -- 
 #BSUB -J YAIB
 ### -- ask for number of cores (default: 1) -- 
@@ -11,11 +11,11 @@
 ### -- specify that the cores must be on the same host -- 
 #BSUB -R "span[hosts=1]"
 ### -- specify that we need 4GB of memory per core/slot -- 
-#BSUB -R "rusage[mem=12GB]"
+#BSUB -R "rusage[mem=4GB]"
 ### -- specify that we want the job to get killed if it exceeds 5 GB per core/slot -- 
-#BSUB -M 12GB
+#BSUB -M 4GB
 ### -- set walltime limit: hh:mm -- 
-#BSUB -W 30:00 
+#BSUB -W 01:00 
 ### -- set the email address -- 
 # please uncomment the following line and put in your e-mail address,
 # if you want to receive e-mail notifications on a non-default address
@@ -26,14 +26,24 @@
 #BSUB -N 
 
 ### -- specify the output and error file inside the run folder -- 
-#BSUB -o /work3/s185395/YAIB/hpc_output/SLL_output_%J.out
-#BSUB -e /work3/s185395/YAIB/hpc_output/SLL_output_%J.err
+#BSUB -o /work3/s185395/YAIB/hpc_output/SL_output_%J.out
+#BSUB -e /work3/s185395/YAIB/hpc_output/SL_output_%J.err
 
 # Activate venv  and load modules 
 module load python3/3.10.16
 source yaib_venv/bin/activate
 
 # Execute command
-
-icu-benchmarks train     -d /work3/s185395/YAIB-cohorts/data/los/eicu_miiv_None     -n eicu_miiv     -t Regression     -tn LOS     -m SSL_BAT_no_tune     -gc     -lc -s 2222     -l ../yaib_logs/ --wandb-sweep
-
+icu-benchmarks \
+  --eval \
+  -d /work3/s185395/YAIB-cohorts/data/mortality24/miiv \
+  -n mimic \
+  -t BinaryClassification \
+  -tn Mortality24 \
+  -m BAT_eval \
+  --generate_cache \
+  --load_cache \
+  -s 2222 \
+  -l ../yaib_logs \
+  -sn mimic \
+  --source-dir /work3/s185395/yaib_logs/mimic/Mortality24/BAT/2025-06-04T11-15-00/repetition_0/fold_0
